dccade18b68c273019c15a51f435985f6181d76c
Grant Revelle <88594909+ghRevelle@users.noreply.github.com>
2025-07-15 18:47:13 -0400

    temporary save

---
+++ src/plane.py
@@ -0,0 +1,15 @@
+import math
+
+class Plane:
+	def __init__(self, init_state: dict):
+		self.state = init_state
+		"""state = {'id' : callsign,
+		            'x' : x position,
+					'y' : y position,
+					'alt' : altitude,
+					'hdg' : heading,
+					'gspd' : ground speed,
+					'v_z' : climb/sink rate}"""
+
+	def tick(self): # advance time by 10 seconds
+		self.state['x'] += self.state['gspd']*math.sin(90 - self.state['hdg'])
