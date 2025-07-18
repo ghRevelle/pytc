5df6786ea070b405ee93933fdf297ebe62f52b50
MrRocket08 <122652107+MrRocket08@users.noreply.github.com>
2025-07-18 16:55:00 -0400

    Added Cessna ascent and descent rate

--- src/plane.py
+++ src/plane.py
@@ -18,6 +18,8 @@ class Plane:
 				'turn_rate' (float): Turn rate of the plane in deg/sec (based only on model)
 				'stall_speed' (float): Plane's minimum speed in m/s (based only on model)
 				'nex_speed' (float): Plane's never-exceed speed in m/s (based only on model)
+				'asc_rate' (float): Plane's maximum no-speed-loss climb rate in m/s (based only on model)
+				'dsc_rate' (float): Plane's maximum descent rate in m/s (asc_rate * 1.5)
 
 				'lat' (float): Latitude position of the plane in degrees.
 				'lon' (float): Longitude position of the plane in degrees.
@@ -48,6 +50,8 @@ class Plane:
 			self.turn_rate = 3
 			self.stall_speed = 32.1014075233 # m/s, or 62.4 kts / 1.94384
 			self.nex_speed = 83.8546382418 # m/s, or 163 kts / 1.94384
+			self.asc_rate = 3.556 # m/s
+			self.dsc_rate = 5.334 # m/s, or 3.556 * 1.5
 
 	# getter-setters
 	def get_state(self) -> object:
