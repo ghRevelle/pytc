ac0b84c8112a17ec16bc3a8c81feb3e071bad56a
SporadicExistentialCrises <dashan.alt.1@gmail.com>
2025-07-25 22:55:19 -0400

    added real life rolling initial state

--- Scripts/main.py
+++ Scripts/main.py
@@ -4,6 +4,7 @@ from plane_manager import PlaneManager
 from airport import Runway, Airport
 from commands import *
 from planestates import PlaneState
+from rolling_initial_state import rolling_initial_state_20250301
 import time
 
 # Base coordinates around San Diego area
@@ -19,68 +20,79 @@ test_runways = {
 
 test_airport = Airport(test_runways)
 
-rolling_initial_state = [
-	{
-		'callsign': 'UA6',
-		'lat': base_lat + 0.15,  # North of the airport
-		'lon': base_lon - 0.5,  # West of the airport
-		'alt': 400,
-		'v_z': 0.0,
-		'gspd': 83.8546382418,
-		'hdg': test_runways[9].hdg,  # Heading towards Runway9
-		'state': PlaneState.AIR,
-		'time_added': 0  # Time added to the simulation
-	},
-	{
-		'callsign': 'UA93',
-		'lat': base_lat + 0.075,  # North of the airport
-		'lon': base_lon - 0.25,  # West of the airport
-		'alt': 400,
-		'v_z': 0.0,
-		'gspd': 83.8546382418,
-		'hdg': test_runways[9].hdg,  # Heading towards Runway9
-		'state': PlaneState.AIR,
-		'time_added': 500  # Time added to the simulation
-	},
-	{
-		'callsign': 'AA11',
-		'lat': base_lat + 0.15,  # North of the airport
-		'lon': base_lon - 0.5,  # West of the airport
-		'alt': 400,
-		'v_z': 0.0,
-		'gspd': 83.8546382418,
-		'hdg': test_runways[9].hdg,  # Heading towards Runway9
-		'state': PlaneState.AIR,
-		'time_added': 20  # Time added to the simulation
-	},
-	{
-		'callsign' : 'RG1',
- 		'lat' : 0.0,
- 		'lon' : 0.0,
- 		'alt' : 0.0,
- 		'v_z' : 0.0,
- 		'gspd' : 0.0,
- 		'hdg' : 0.0,
- 		'state' : PlaneState.QUEUED,
-		'time_added' : 0  # Time added to the simulation
- 	}
-]
+# rolling_initial_state = [
+# 	{
+# 		'callsign': 'UA6',
+# 		'lat': base_lat + 0.15,  # North of the airport
+# 		'lon': base_lon - 0.5,  # West of the airport
+# 		'alt': 400,
+# 		'v_z': 0.0,
+# 		'gspd': 83.8546382418,
+# 		'hdg': test_runways[9].hdg,  # Heading towards Runway9
+# 		'state': PlaneState.AIR,
+# 		'time_added': 0  # Time added to the simulation
+# 	},
+# 	{
+# 		'callsign': 'UA93',
+# 		'lat': base_lat + 0.075,  # North of the airport
+# 		'lon': base_lon - 0.25,  # West of the airport
+# 		'alt': 400,
+# 		'v_z': 0.0,
+# 		'gspd': 83.8546382418,
+# 		'hdg': test_runways[9].hdg,  # Heading towards Runway9
+# 		'state': PlaneState.AIR,
+# 		'time_added': 500  # Time added to the simulation
+# 	},
+# 	{
+# 		'callsign': 'AA11',
+# 		'lat': base_lat + 0.15,  # North of the airport
+# 		'lon': base_lon - 0.5,  # West of the airport
+# 		'alt': 400,
+# 		'v_z': 0.0,
+# 		'gspd': 83.8546382418,
+# 		'hdg': test_runways[9].hdg,  # Heading towards Runway9
+# 		'state': PlaneState.AIR,
+# 		'time_added': 20  # Time added to the simulation
+# 	},
+# 	{
+# 		'callsign' : 'RG1',
+#  		'lat' : 0.0,
+#  		'lon' : 0.0,
+#  		'alt' : 0.0,
+#  		'v_z' : 0.0,
+#  		'gspd' : 0.0,
+#  		'hdg' : 0.0,
+#  		'state' : PlaneState.QUEUED,
+# 		'time_added' : 0  # Time added to the simulation
+#  	}
+# ]
+
+rolling_initial_state = []
+
+for state in rolling_initial_state_20250301:
+	rolling_initial_state.append(state.copy())
+	if rolling_initial_state[-1]['state'] == 'takeoff':
+		rolling_initial_state[-1]['state'] = PlaneState.QUEUED
+	elif rolling_initial_state[-1]['state'] == 'landing':
+		rolling_initial_state[-1]['state'] = PlaneState.AIR
+
+print(rolling_initial_state[:10])
 
 fs = FlightSimulator(display_size=(900, 900), airport = test_airport, plane_manager = PlaneManager(), rolling_initial_state=rolling_initial_state)
 
 runway = test_runways[9]
 
-fs.add_command_by_callsign('UA6', CommandType.REALIGN, last_update=200, argument=runway)
-fs.add_command_by_callsign('UA6', CommandType.CLEARED_TO_LAND, last_update=300, argument=runway)
+# fs.add_command_by_callsign('UA6', CommandType.REALIGN, last_update=200, argument=runway)
+# fs.add_command_by_callsign('UA6', CommandType.CLEARED_TO_LAND, last_update=300, argument=runway)
 
-fs.add_command_by_callsign('UA93', CommandType.REALIGN, last_update=600, argument=runway)
-fs.add_command_by_callsign('UA93', CommandType.CLEARED_TO_LAND, last_update=700, argument=runway)
+# fs.add_command_by_callsign('UA93', CommandType.REALIGN, last_update=600, argument=runway)
+# fs.add_command_by_callsign('UA93', CommandType.CLEARED_TO_LAND, last_update=700, argument=runway)
 
-fs.add_command_by_callsign('AA11', CommandType.REALIGN, last_update=200, argument=runway)
-fs.add_command_by_callsign('AA11', CommandType.CLEARED_TO_LAND, last_update=300, argument=runway)
+# fs.add_command_by_callsign('AA11', CommandType.REALIGN, last_update=200, argument=runway)
+# fs.add_command_by_callsign('AA11', CommandType.CLEARED_TO_LAND, last_update=300, argument=runway)
 
-fs.add_command_by_callsign('RG1', CommandType.LINE_UP_AND_WAIT, last_update=500, argument=runway)
-fs.add_command_by_callsign('RG1', CommandType.CLEARED_FOR_TAKEOFF, last_update=550, argument=runway)
+# fs.add_command_by_callsign('RG1', CommandType.LINE_UP_AND_WAIT, last_update=500, argument=runway)
+# fs.add_command_by_callsign('RG1', CommandType.CLEARED_FOR_TAKEOFF, last_update=550, argument=runway)
 
 for i in range(2500):
 	# Run the simulation for 2500 ticks
