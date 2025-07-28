0da4475192dd4f65bdb5f513e309dbf880382f6b
SporadicExistentialCrises <dashan.alt.1@gmail.com>
2025-07-27 21:29:47 -0400

    debug changes to main

--- Scripts/main.py
+++ Scripts/main.py
@@ -24,7 +24,7 @@ test_airport = Airport(test_runways)
 rolling_initial_state = []
 
 # Convert the rolling initial state states to use PlaneState enums
-for state in rolling_initial_state_01:
+for state in rolling_initial_state_00:
 	rolling_initial_state.append(state.copy())
 	if rolling_initial_state[-1]['state'] == 'takeoff':
 		rolling_initial_state[-1]['state'] = PlaneState.QUEUED
@@ -43,13 +43,18 @@ fs = FlightSimulator(display_size=(900, 900), airport = test_airport, plane_mana
 
 runway = test_runways[27]
 
-# fs.add_command_by_callsign('BAW82P', CommandType.REALIGN, last_update=10, argument=runway)
 # fs.add_command_by_callsign('BAW82P', CommandType.CLEARED_TO_LAND, last_update=80, argument=runway)
 
 for i in range(2500):
 	# # Run the simulation for 2500 ticks
-	if i >= 1400 and i <= 1800 and i % 100 == 0:
-		target_id = copy.deepcopy(fs.plane_manager.airport.get_top_of_queue())
-		fs.add_command(Command(command_type=CommandType.LINE_UP_AND_WAIT, target_id=target_id, last_update=i+1, argument=runway))
-		fs.add_command(Command(command_type=CommandType.CLEARED_FOR_TAKEOFF, target_id=target_id, last_update=i+2, argument=runway))
+	for state in rolling_initial_state:
+		if state['time_added'] == i and state['state'] == PlaneState.AIR:
+			fs.add_command_by_callsign(state['callsign'], CommandType.CLEARED_TO_LAND, last_update = i+79, argument=runway)
+		elif state['time_added'] == i and state['state'] == PlaneState.QUEUED:
+			fs.add_command_by_callsign(state['callsign'], CommandType.LINE_UP_AND_WAIT, last_update = i+1, argument=runway)
+			fs.add_command_by_callsign(state['callsign'], CommandType.CLEARED_FOR_TAKEOFF, last_update = i+2, argument=runway)
+	# if i >= 1400 and i <= 1800 and i % 100 == 0:
+	# 	target_id = copy.deepcopy(fs.plane_manager.airport.get_top_of_queue())
+	# 	fs.add_command(Command(command_type=CommandType.LINE_UP_AND_WAIT, target_id=target_id, last_update=i+1, argument=runway))
+	# 	fs.add_command(Command(command_type=CommandType.CLEARED_FOR_TAKEOFF, target_id=target_id, last_update=i+2, argument=runway))
 	fs.tick()
